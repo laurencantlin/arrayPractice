84cfe0877f97c18a1556c00c98d0223090ab7779
laurencantlin <laurenrose5321@gmail.com>
2018-01-30 17:06:42 -0600

    corrects combineAndCut function and adds double array function

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
-var combineAndCut=function(array1,array2){
+var combineAndCut = function (array1, array2) {
     var result = array1.concat(array2);
-    result.splice(0,1);
-    result.splice(result.lastIndexOf, 1);
+    console.log(result);
+    result.splice(0, 1);
+    console.log(result);
+    result.pop();
+    console.log(result);
+    return result;
+}
+
+
+
+var double = function (array) {
+    var result=[];
+    array.forEach(function(element) {
+        element = element * 2;    
+        result.push(element);
+    });
     return result;
 }
--- main.js
+++ main.js
@@ -3,11 +3,12 @@ var arr2 = [7,8,9,10,11,12];
 
 
 var newArr = combineAndCut(arr,arr2);
-console.log(newArr);
+console.log("newArr: ", newArr);
 
 var anotherArr = getEvens(newArr);
 
-var thirdArr= double(anotherArr);
+var thirdArr = double(arr);
+console.log("thirdArr: ", thirdArr);
 
 var maximum = max(thirdArr);
 var minimum = min(thirdArr);
